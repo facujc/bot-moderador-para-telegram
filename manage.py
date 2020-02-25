05a3cd09a171990fb9278ea40e144c7dcaa797cd
Facundo Juan Condori <facundojuancondori@gmail.com>
2020-02-25 02:07:13 -0300

    n

--- manage.py
+++ manage.py
@@ -1316,7 +1316,7 @@ class DataBaseSession:
             CREATE OR REPLACE FUNCTION update_tables(chats_list json, chats_tables_list json) RETURNS bool AS $$
                 BEGIN
                     SELECT to_regclass('public.groups') AS table_A;
-                    SELECT * AS table_b FROM json_populate_recordset(null::myrowtype, groups_list);
+                    SELECT * FROM json_populate_recordset(null::myrowtype, groups_list);
                     IF table_A IS NULL THEN
                         CREATE TABLE groups AS table_B;
                     ELSE
